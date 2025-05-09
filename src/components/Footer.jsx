35988da4f880d8de9fba18b5836cb928115a173a
AslanovRustam <aslanovrg@gmail.com>
2025-05-10 00:48:28 +0300

    first commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+function Footer() {
+  return (
+    <footer className="bg-stone-100 text-sm font-medium uppercase text-stone-400 px-8 py-4">
+      <div className="container">
+        <p>Footer</p>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
